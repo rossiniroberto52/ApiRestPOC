555e5e7568fce9f9906dc36ae2f35ac8de1ef1a8
Rossini Roberto <toquehuk@gmail.com>
2022-08-25 21:21:29 -0300

    update 1.0

---
+++ AR.Data/MainContext.cs
@@ -0,0 +1,15 @@
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AR.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Client> Client {get; set;}
+    }
+}
---
+++ AR.Domain/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Client
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} carcteres")]
+        public string Name { get; set; }
+        public string cpf { get; set; }
+        public int Idade { get; set; }
+    }
+}
